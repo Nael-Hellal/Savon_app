cb93788c9acaaf7db0383af9ee42bb594b52927c
nael.hellal <nael.hella.pro@gmail.com>
2026-03-12 09:47:08 +0100

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
