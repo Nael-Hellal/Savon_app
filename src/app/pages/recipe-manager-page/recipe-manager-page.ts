1d1ee66e2998bb8511159233221b8d60afecfa6f
nael.hellal <nael.hella.pro@gmail.com>
2026-03-16 12:00:33 +0100

    mise a jour

--- src/app/pages/recipe-manager-page/recipe-manager-page.ts
+++ src/app/pages/recipe-manager-page/recipe-manager-page.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Recette } from '../../src/app/models/recette.model';
+import { RecetteServiceTs } from '../../services/recette.service.ts';
 
 @Component({
   selector: 'app-recipe-manager-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-manager-page.html',
   styleUrl: './recipe-manager-page.css',
 })
-export class RecipeManagerPage {
+export class RecipeManagerPage implements OnInit {
+  public recetteSelectionnee: Recette | null = null;
+  public recettes: Recette[] = [];
+  constructor(private recetteServiceTs: RecetteServiceTs) { }
+  ouvrirModale(recette: Recette): void {
+    this.recetteSelectionnee = recette;
+  }
 
-}
+  ngOnInit(): void {
+    this.recetteServiceTs.getRecettes();
+  }
+
+  getRecettes(): void {
+    this.recetteServiceTs.getRecettes().subscribe({
+      next: (data) => {
+        this.recettes = data;
+        console.log("Recettes récupérés avec succès !")
+      },
+      error: (err) => {
+        console.error("Erreur API : ", err);
+      }
+    }
+    )
+    supprimerRecette(id: number): void {
+      if(confirm("Supprimer cet recettes ?")) {
+      this.recetteServiceTs.supprimerRecette(id).subscribe(() =>
+        this.getRecettes());
+    }
+    /**
+    * Réinitialise la sélection à la fermeture
+    */
+    fermerModale(): void {
+      this.recetteSelectionnee = null;
+    }
+
+  }
